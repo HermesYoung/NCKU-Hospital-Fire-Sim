779e7961b156007c6ce09a37c1ffa72c3f021e30
HermesYoung <tails777tw@gmail.com>
2018-05-14 00:39:47 +0800

    Script update

---
+++ Assets/Script/Movement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+    public Transform fl;
+	// Use this for initialization
+	void Start () {
+        fl = GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        fl.rotation = new Quaternion(fl.rotation.x, fl.rotation.y + Input.GetAxis("Mouse X"), fl.rotation.z, fl.rotation.w);
+		
+	}
+}
