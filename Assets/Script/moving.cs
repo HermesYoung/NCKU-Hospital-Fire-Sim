9d5a8bd49dd9466049de3f77acda96948afc4a40
HermesYoung <tails777tw@gmail.com>
2018-05-15 20:05:42 +0800

    add collsion

--- Assets/Script/moving.cs
+++ Assets/Script/moving.cs
@@ -8,11 +8,14 @@ public class moving : MonoBehaviour {
 	public float rotateX, rotateY;
 	public  float speed;
 	private CharacterController controller;
+    public Transform charaMove;
+   
 
 	// Use this for initialization
 	void Start () {
 		controller = GetComponent<CharacterController> ();
-
+        cameraPos = GetComponent<Transform>();
+        charaMove = GetComponent<Transform>();
 	}
 	
 	// Update is called once per frame
@@ -20,8 +23,13 @@ public class moving : MonoBehaviour {
 		rotateX += Input.GetAxis ("Mouse X") * sensitivity;
 		rotateY -= Input.GetAxis ("Mouse Y") * sensitivity;
 		rotateY = Mathf.Clamp (rotateY, -30f, 30f);
-		cameraPos.LookAt (transform);
-		transform.localRotation = Quaternion.Euler (rotateY, rotateX, 0);
+		//cameraPos.LookAt (transform);
+	   charaMove.localRotation = Quaternion.Euler (0, rotateX, 0);
+        cameraPos.localRotation = Quaternion.Euler(rotateY, rotateX, 0);
+        if (Input.GetKey(KeyCode.W))
+        {
+            charaMove.localPosition = Vector3.MoveTowards(charaMove.position, charaMove.position + transform.forward * 2, speed);
+        }
 		/*
 		float moveLR = Input.GetAxis ("Horizontal") * speed;
 		float moveFB = Input.GetAxis ("Vertical") * speed;
