eb3b047b601f5131e550832f5bae5d0446afea15
yakiyang <ninayang1104@gmail.com>
2018-05-19 10:15:36 +0800

    commit

---
+++ Assets/Script/RightHandInput.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RightHandInput : MonoBehaviour {
+
+    public float speed = 0.0f;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float x = Input.GetAxis("TriggerRightHorizontal");
+        float y = Input.GetAxis("TriggerRightVertical");
+        x = x * Time.deltaTime * speed;
+        y = y * Time.deltaTime * speed;
+
+        transform.Translate(x ,y ,0f);
+	}
+}
