68df36ead03e371109f0677d1797ae77b671da20
yakiyang <ninayang1104@gmail.com>
2018-05-24 16:20:38 +0800

    commit

---
+++ Assets/Script/DeleteBtn.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class DeleteBtn : MonoBehaviour {
+    public Text OutText;
+    Button btn;
+	// Use this for initialization
+	void Start () {
+        btn = GetComponent<Button>();
+        btn.onClick.AddListener(delegate
+        {
+            BtnClick();
+        });
+	}
+    void BtnClick()
+    {
+        OutText.text = "";
+    }
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
