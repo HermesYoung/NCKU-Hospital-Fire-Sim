740236acd718dc6f2ba92ad66e2dac25e3f19fdc
yakiyang <ninayang1104@gmail.com>
2018-06-06 01:12:27 +0800

    dialogue , workselectUI change, people from fire circle animation(illustration)

---
+++ Assets/Script/PersonMove.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersonMove : MonoBehaviour {
+    public Animator person;
+	// Use this for initialization
+	void Start () {
+	}
+	public void  Move(){
+        person.SetBool("isMove", true);
+    }
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Script/dialogue.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dialogue : MonoBehaviour {
+
+    public GameObject s1;
+    public GameObject s2;
+    public GameObject s3;
+    public GameObject s4;
+    public GameObject UI;
+    int count = 0;
+	// Use this for initialization
+	void Start () {
+        s1.SetActive(false);
+        s2.SetActive(false);
+        s3.SetActive(false);
+        s4.SetActive(false);
+	}
+	public void get_sentence(){
+        count++;
+        print();
+    }
+    void print()
+    {
+        switch (count)
+        {
+            case 1:
+                s1.SetActive(true);
+                break;
+            case 2:
+                s2.SetActive(true);
+                break;
+            case 3:
+                s3.SetActive(true);
+                break;
+            case 4:
+                s4.SetActive(true);
+                break;
+            default:
+                UI.SetActive(false);
+                break;
+        }
+    }
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
