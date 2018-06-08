1ef3ec33da6250e1419ec56ff7c369ab821b814c
yakiyang <ninayang1104@gmail.com>
2018-06-08 20:55:58 +0800

    start scene ,add Emergency department

---
+++ Assets/Script/SwitchSceneArea.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SwitchSceneArea : MonoBehaviour {
+	bool istouch = false;
+	bool istrigger = false;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	public void Switch(){
+		if(istouch && istrigger){
+			SceneManager.LoadScene("area");
+		}
+	}
+	public void Touch(){
+		istouch = true;
+		Switch();
+	}
+	public void NotTouch(){
+		istouch = false;
+	}
+	public void Trigger(){
+		istrigger = true;
+		Switch();
+	}
+	public void NotTrigger(){
+		istrigger = false;
+	}
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
